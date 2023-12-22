8765a0d38c76f36d782a9bceda17f87b348cda36
Braaaboarshed <102403208+Braaaboarshed@users.noreply.github.com>
2023-12-22 22:26:04 +0300

    Update connactTodb.js

--- backend/config/connactTodb.js
+++ backend/config/connactTodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = async () =>{
-  //   try{
+    // try{
         
   //       await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: "true",
   // useUnifiedTopology: "true"});
@@ -9,7 +9,7 @@ module.exports = async () =>{
   //   }catch(error){
   //       console.log('connaction falild to mongo db',error)
   //   }
-    await mongoose.connect(process.env.MONGO_URI, {
+     mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology:true,
       useCreateIndex: true
